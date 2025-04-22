4f06261fb7f2cb09ba3b85eba34344ece207234e
Arturooiwnl <andresperotto71@gmail.com>
2025-04-22 19:27:55 -0300

    Add SEO metadata and description to the layout

---
+++ src/layouts/constLayout.ts
@@ -0,0 +1,3 @@
+export const description = "Visita mi portafolio y descubre mis proyectos y habilidades."
+export const siteName = "Arturoiwnl | Portafolio. Programador y Desarrollador Web."
+export const imageMetaAlt = "Portafolio de Arturoiwnl, programador y desarrollador web."
